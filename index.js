b74c4986c2689b011854f76230241b96df62ab9c
yeongsuhan <amazingmobdev@gmail.com>
2020-06-29 17:52:39 +0900

    add some props

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import {Text, View, Animated} from 'react-native';
+import { Text, View, Animated, Easing } from 'react-native';
 
 const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const AnimatedNumber = ({animatedNumber, fontStyle, animationDuration}) => {
+const AnimatedNumber = ({
+  animatedNumber,
+  fontStyle,
+  animationDuration,
+  includeComma,
+  startWithZero,
+  easing,
+}) => {
   const animatedNumbersArr = Array.from(String(animatedNumber), Number);
-  const reducedArray = new Array(
-    Math.ceil(String(animatedNumber).length / 3),
-  ).fill(0);
 
-  reducedArray.map((__, index) => {
-    if (index === 0) {
-      return;
-    }
+  if (includeComma) {
+    const reducedArray = new Array(
+      Math.ceil(String(animatedNumber).length / 3),
+    ).fill(0);
 
-    animatedNumbersArr.splice(
-      String(animatedNumber).length - index * 3,
-      0,
-      ',',
-    );
-  });
+    reducedArray.map((__, index) => {
+      if (index === 0) {
+        return;
+      }
+
+      animatedNumbersArr.splice(
+        String(animatedNumber).length - index * 3,
+        0,
+        ',',
+      );
+    });
+  }
 
   const [numberHeight, setNumberHeight] = React.useState(0);
   const animations = animatedNumbersArr.map((__, index) => {
-    const animationHeight =
-      -1 * (numberHeight * Math.floor(Math.random() * 10));
+    const animationHeight = startWithZero
+      ? 0
+      : -1 * (numberHeight * Math.floor(Math.random() * 10));
     return new Animated.Value(animationHeight);
   });
 
@@ -42,6 +53,7 @@ const AnimatedNumber = ({animatedNumber, fontStyle, animationDuration}) => {
         toValue: -1 * (numberHeight * animatedNumbersArr[index]),
         duration: animationDuration || 1400,
         useNativeDriver: true,
+        easing: easing || Easing.elastic(1.2),
       }).start();
     });
   }, [animatedNumber, numberHeight]);
@@ -53,18 +65,20 @@ const AnimatedNumber = ({animatedNumber, fontStyle, animationDuration}) => {
   return (
     <>
       {numberHeight !== 0 && (
-        <View style={{flexDirection: 'row'}}>
+        <View style={{ flexDirection: 'row' }}>
           {animatedNumbersArr.map((n, index) => {
             if (typeof n === 'string') {
               return (
-                <Text key={index} style={[fontStyle, {height: numberHeight}]}>{n}</Text>
+                <Text key={index} style={[fontStyle, { height: numberHeight }]}>
+                  {n}
+                </Text>
               );
             }
 
             return (
               <View
                 key={index}
-                style={{height: numberHeight, overflow: 'hidden'}}>
+                style={{ height: numberHeight, overflow: 'hidden' }}>
                 <Animated.View
                   style={[
                     {
@@ -76,8 +90,8 @@ const AnimatedNumber = ({animatedNumber, fontStyle, animationDuration}) => {
                     },
                   ]}>
                   {NUMBERS.map((number, i) => (
-                    <View style={{flexDirection: 'row'}} key={i}>
-                      <Text style={[fontStyle, {height: numberHeight}]}>
+                    <View style={{ flexDirection: 'row' }} key={i}>
+                      <Text style={[fontStyle, { height: numberHeight }]}>
                         {number}
                       </Text>
                     </View>
@@ -89,7 +103,7 @@ const AnimatedNumber = ({animatedNumber, fontStyle, animationDuration}) => {
         </View>
       )}
       <Text
-        style={[fontStyle, {position: 'absolute', top: -999999}]}
+        style={[fontStyle, { position: 'absolute', top: -999999 }]}
         onLayout={setButtonLayout}>
         {0}
       </Text>
