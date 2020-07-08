e901236d4e8984ee302665b9990a003541417b0f
yeongsuhan <amazingmobdev@gmail.com>
2020-07-08 17:40:20 +0900

    test2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, Animated, Easing } from 'react-native';
 
-const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const AnimatedNumber = ({
 	animateToNumber,
