40bb814051ac0a0a687ae76ee666f1dba60932c4
youngsoohan <amazingmobdev@gmail.com>
2020-08-22 00:03:26 +0900

    change eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,24 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-	'env': {
-		'browser': true,
-		'es2020': true
+	env: {
+		browser: true,
+		es2020: true,
 	},
-	'extends': [
-		'eslint:recommended',
-		'plugin:react/recommended'
-	],
-	'parserOptions': {
-		'ecmaFeatures': {
-			'jsx': true
+	extends: ['eslint:recommended', 'plugin:react/recommended'],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
 		},
-		'ecmaVersion': 11,
-		'sourceType': 'module',
+		ecmaVersion: 11,
+		sourceType: 'module',
 	},
-	'plugins': [
-		'react'
-	],
-	'rules': {
+	plugins: ['react'],
+	ignorePatterns: ['ExampleProject/'],
+	rules: {
 		'react/prop-types': 0,
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'always'
-		]
-	}
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always'],
+	},
 };
