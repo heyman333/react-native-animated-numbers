db811fba3f8dfedbbc981395388a987d6827a04f
Huckleberry <huckleberry@wrtn.io>
2024-04-12 21:03:57 +0900

    refactor: recreate ExampleProject with expo

--- ExampleProject/App.tsx
+++ ExampleProject/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import {SafeAreaView, Button} from 'react-native';
-import AnimatedNumbers from 'react-native-animated-numbers';
+import React from "react";
+import { SafeAreaView, Button } from "react-native";
+import AnimatedNumbers from "react-native-animated-numbers";
 
 const App = () => {
   const [animateToNumber, setAnimateToNumber] = React.useState(483);
@@ -9,17 +9,18 @@ const App = () => {
     setAnimateToNumber(animateToNumber + 391);
   };
 
-  const decrease = () => { 
-    setAnimateToNumber(animateToNumber - 311)
-  }
+  const decrease = () => {
+    setAnimateToNumber(animateToNumber - 311);
+  };
 
   return (
     <SafeAreaView
-      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+    >
       <AnimatedNumbers
         includeComma
         animateToNumber={animateToNumber}
-        fontStyle={{fontSize: 50, fontWeight: 'bold'}}
+        fontStyle={{ fontSize: 50, fontWeight: "bold" }}
       />
       <Button title="increase" onPress={increase} />
       <Button title="decrease" onPress={decrease} />
