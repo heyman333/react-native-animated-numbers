a3b27b927185b050fbe8a6dad2176b8dbe3a781f
Huckleberry <huckleberry@wrtn.io>
2024-04-12 20:49:54 +0900

    refactor: clean up deps & set typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-	env: {
-		browser: true,
-		es2020: true,
-	},
-	extends: ['eslint:recommended', 'plugin:react/recommended'],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 11,
-		sourceType: 'module',
-	},
-	plugins: ['react'],
-	ignorePatterns: ['ExampleProject/'],
-	rules: {
-		'react/prop-types': 0,
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-	},
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  ignorePatterns: ["ExampleProject/"],
+  rules: {
+    "react/prop-types": 0,
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+  },
 };
